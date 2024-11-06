fab2ddce55fecbba1a8ea029e6da30897150c5e1
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-05 19:02:24 -0500

    Update headers.h

--- src/1.2_hide_lsmod/headers.h
+++ src/1.2_hide_lsmod/headers.h
@@ -1,6 +1,11 @@
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/kallsyms.h>
 #include <linux/version.h>
+
+#endif
