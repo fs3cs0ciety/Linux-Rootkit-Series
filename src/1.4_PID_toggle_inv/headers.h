d1d102fa6ea9e67c0b2ef60cbab2ea7ef64fdf89
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-07 16:43:57 -0500

    Create headers.h

---
+++ src/1.4_PID_toggle_inv/headers.h
@@ -0,0 +1,12 @@
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/dirent.h>
+#include <linux/version.h>
+
+#endif
